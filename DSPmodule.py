30e4cc77245a06cae658f540dfde82c6da851cae
DavidLoon <daveloon@gmail.com>
2014-12-01 20:27:50 +0000

    more changes to standardize
    more changes to standardize

--- DSPmodule.py
+++ DSPmodule.py
@@ -1,7 +1,6 @@
 # function which standardizes list or array to zero mean and unit variance
 import numpy as np
 def standardize(x):
-
 	try:	
 		mean_x=np.mean(x)
 		x=x-mean_x
@@ -11,3 +10,4 @@ def standardize(x):
 	except:
 		print 'Error.'
 		return False
+
