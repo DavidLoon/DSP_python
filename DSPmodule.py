75bc3b92a38bfe1c7ec25e8b1577c4ab928299fd
DavidLoon <daveloon@gmail.com>
2014-12-01 20:12:35 +0000

    Addition of DSP module
    Addition of DSP module

---
+++ DSPmodule.py
@@ -0,0 +1,13 @@
+# function which standardizes list or array to zero mean and unit variance
+import numpy as np
+def standardize(x):
+
+	try:	
+		mean_x=np.mean(x)
+		x=x-mean_x
+		std_x=np.std(x)
+		x=x/std_x
+		return x
+	except:
+		print 'Error.'
+		return False
